07e9d9c22358f3fd5f9ca023c3d5dfde7bbd9429
Jan Simson <jan.simson@three-2-one.com>
2021-10-17 14:05:28 +0200

    Set flag re unwrapping injections

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 // import dataPlugin from './plugins/data.js'
 
 const instance = createApp(App)
+instance.config.unwrapInjectedRef = true
 
 // instance.use(dataPlugin)
 
