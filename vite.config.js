cacb74ba5ffaf77eab9df25572f98184a5e27d49
Jan Simson <jan.simson@three-2-one.com>
2021-11-11 22:08:10 +0000

    Fix build error due to missing bigint support

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    // Use newer build target here to deal with bigint64 not being supported in all browsers!
+    target: ['es2020']
+  }
 })
