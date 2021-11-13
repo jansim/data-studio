ffa7552ea304b8a33c7d6a77fb2ae30ed1787000
Jan Simson <jan.simson@three-2-one.com>
2021-11-13 09:30:37 +0000

    Ensure asset filenames don’t start with a dot to fix an issue with netlify deployments

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     // Use newer build target here to deal with bigint64 not being supported in all browsers!
-    target: ['es2020']
+    target: ['es2020'],
+    rollupOptions: {
+      output: {
+        // Use an underscore here instead of dot between name and hash to avoid
+        // an issue where netlify won't deploy files starting with a dot
+        assetFileNames: `assets/[name]_[hash].[ext]`
+      }
+    }
   }
 })
