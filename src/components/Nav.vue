ebac290bddb461e6d61fb39ba46e90bf2cdc339c
Jan Simson <jan.simson@three-2-one.com>
2021-12-05 12:48:46 +0100

    Add missing Nav compoonent from previous commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <img class="logo" alt="Data Studio Logo" title="Data Studio" src="../assets/logo.svg"/>
+
+    <slot/>
+  </nav>
+</template>
+
+<script>
+
+</script>
+
+<style lang="css" scoped>
+nav {
+  display: flex;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.2);
+}
+
+.logo {
+  width: 2rem;
+  padding-left: 0.75rem;
+}
+</style>
